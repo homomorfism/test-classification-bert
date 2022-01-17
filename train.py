ba868725be9cd79ce19448a128478751860ca937
homomorfism <arsl.sh056@mail.ru>
2022-01-17 21:48:23 +0300

    fix(kaggle): fixing training on kaggle

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import pytorch_lightning as pl
 from easydict import EasyDict
 from omegaconf import DictConfig
 from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
 
 from src.dataloader import ClassificationDataLoader
 from src.model import BertModel
@@ -18,6 +19,7 @@ def train(cfg: DictConfig):
     cfg = EasyDict(cfg)
 
     data_dir = Path(cfg.data.data_dir)
+    current_dir = Path(Path(cfg.data.current_dir))
     train_df = pd.read_csv(data_dir / "train.csv")
     test_df = pd.read_csv(data_dir / "test.csv")
 
@@ -30,7 +32,16 @@ def train(cfg: DictConfig):
         **cfg.data
     )
 
-    checkpointer = ModelCheckpoint(dirpath=Path(cfg.data.current_dir) / "models",
+    logger = WandbLogger(name="baseline_bert",
+                         save_dir=str(current_dir / "logs"),
+                         log_model=True,
+                         experiment="Five epochs",
+                         project="contradictory-my-dear-watson",
+                         config=cfg)
+
+    logger.watch(model, log_freq=1000)
+
+    checkpointer = ModelCheckpoint(dirpath=current_dir / "models",
                                    monitor="val/loss_epoch",
                                    mode="min",
                                    save_weights_only=True,
@@ -38,11 +49,13 @@ def train(cfg: DictConfig):
                                    filename=r"bert_{epoch}-{val_loss_epoch:.02f}")
 
     trainer = pl.Trainer(
+        logger=logger,
         callbacks=[checkpointer],
         gpus=cfg.gpus,
         max_epochs=cfg.model.num_epochs,
-        limit_train_batches=2,
-        limit_val_batches=2,
+        default_root_dir=str(current_dir / "logs"),
+        # limit_train_batches=2,
+        # limit_val_batches=2,
     )
 
     trainer.fit(model, dataloader.train_dataloader(), dataloader.val_dataloader())
